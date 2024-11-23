2dad6b20501cb4a171bc6019782eafdc8a9794cf
Simon H-M <simonhm31@gmail.com>
2024-11-23 09:57:38 -0600

    tests

--- disemvowel/main.rs
+++ disemvowel/main.rs
@@ -1,82 +1,32 @@
-pub mod disemvowel;
-
-use crate::disemvowel::disemvowel;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Write};
 use std::env;
-use std::fs;
-use std::path::Path;
 
-fn main() {
-    // The first command line argument is the name of the executable
-    // that was called to run this program. After that should be
-    // two command line arguments, both file names. The first should
-    // be the name of a file containing the text to disemvowel,
-    // and the second should be the file we want to write the disemvoweled text to.
+fn is_vowel(c: char) -> bool {
+    matches!(c, 'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U')
+}
+
+fn disemvowel(s: &str) -> String {
+    s.chars().filter(|&c| !is_vowel(c)).collect()
+}
+
+fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} <file_path>", args[0]);
+        std::process::exit(1);
+    }
+    let file_path = &args[1];
 
-    //TODO: Panic if not enough arguments are provided
-    //Panic should output the string "Not enough arguments"
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+    let mut stdout = io::stdout();
 
-    //TODO:
-    //  * Pass an argument to read_file to read the original text
-    //  * Pass that to disemvowel to remove the vowels
-    //  * Write the disemvoweled text using write_file
-
-    // Replace String::from("dummy text") with what you get from read_file
-    let s = String::from("dummy text");
-
-    let s_disemvowel = disemvowel(&s);
-
-    // Use command-line arguments for the name of the file,
-    // and s_disemvowel for the text to write out.
-    write_file(Path::new("dummy.txt"), "output string");
-}
-
-fn read_file(path: &Path) -> String {
-    fs::read_to_string(path).expect("Could not read the file")
-}
-
-fn write_file(path: &Path, s: &str) {
-    fs::write(path, s).expect("Unable to write file");
-}
-
-// Everything from here down is Rust test code. You shouldn't need to
-// change any of this.
-//
-// Use `cargo test` to run all these tests. All the tests will initially
-// fail because there's no definition for the `disemvowel` function. Add
-// that up above and work to get the tests to pass. See the lab write-up
-// for some tips.
-
-// Tests that check that the correct panics are generated when
-// there aren't the correct number of command line arguments
-// or the input file isn't readable.
-#[cfg(test)]
-mod tests {
-    use assert_cmd::{assert::OutputAssertExt, cargo::CommandCargoExt};
-    use predicates::prelude::predicate;
-    use std::process::Command;
-
-    #[test]
-    fn requires_two_arguments() {
-        // The `Command` library lets you create and run a CLI command.
-        let mut cmd = Command::cargo_bin("disemvowel-in-rust").unwrap();
-        // Add one command-line argument to this command, namely "1". The assertion
-        // below should fail because we didn't provide two arguments as required.
-        cmd.arg("1");
-        cmd.assert()
-            .failure()
-            .stderr(predicate::str::contains("Not enough arguments"));
+    for line in reader.lines() {
+        let line = line?;
+        let result = disemvowel(&line);
+        writeln!(stdout, "{}", result)?;
     }
 
-    #[test]
-    fn requires_read_file() {
-        let mut cmd = Command::cargo_bin("disemvowel-in-rust").unwrap();
-        // Provide two arguments, but both as paths to files that don't actually exist.
-        // The assertion below will fail because we couldn't open the first file.
-        cmd.arg("/this/path/does/not/exist")
-            .arg("output/path/does/not/matter");
-        cmd.assert()
-            .failure()
-            .stderr(predicate::str::contains("Could not read the file"));
-    }
+    Ok(())
 }
